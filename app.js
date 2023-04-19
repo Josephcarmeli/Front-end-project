49c26a9ce9f83ebcaccdf8838272ef2f788c797e
JosephCarmeli <josephcarmeli@hotmail.com>
2023-04-19 14:03:15 -0500

    added css and location feature

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ let nameEl = document.querySelector(".name");
 let descEl = document.querySelector(".descEl");
 let tempEl = document.querySelector(".temp");
 let selectedUnit = document.querySelector('input[name="units"]:checked').value;
+let currentLocationBtn = document.querySelector(".location-button")
 
 document.querySelectorAll('input[name="units"]').forEach(function(radio) {
     radio.addEventListener("change", function() {
       selectedUnit = this.value;
-      // Call API again with updated unit
       button.click();
     });
   });
   
-  button.addEventListener("click", function() {
+  button.addEventListener("click", () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=3b198bf6856a18a221a056cee186c32a&units=${selectedUnit}`)
       .then(function (response) {
         console.log(response);
@@ -28,4 +28,29 @@ document.querySelectorAll('input[name="units"]').forEach(function(radio) {
       .catch(function (error) {
         console.log(error);
       });
+  });
+
+  currentLocationBtn.addEventListener("click", () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3b198bf6856a18a221a056cee186c32a&units=${selectedUnit}`)
+                .then(function (response) {
+                    console.log(response);
+                    nameEl.textContent = response.data.name;
+                    descEl.textContent = response.data.weather[0].description;
+                    if (selectedUnit === "metric") {
+                        tempEl.textContent = response.data.main.temp + "°C";
+                    } else {
+                        tempEl.textContent = response.data.main.temp + "°F";
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.")
+    }
   });
